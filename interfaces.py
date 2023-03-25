178ff3f42a36ba891095b1cd5712802aa0c5c377
pyrus369 <nmg860@my.utsa.edu>
2023-03-25 14:12:58 -0500

    commit

--- interfaces.py
+++ interfaces.py
@@ -4,21 +4,47 @@ import os
 
 #creating a blank list to store the output
 wifi_interfaces = []
+wifi_interfaces2 = []
+wifi_networks = []
 
 #running the command to get the wifi interfaces
 #output = subprocess.check_output(["ipconfig"])
 #output = subprocess.check_output(["netsh wlan show interfaces"])
 output = subprocess.check_output("netsh wlan show interfaces")
+output2 = subprocess.check_output("netsh wlan show networks")
 #print(repr(output))
 #print(output)
+'''
 with open("output.txt", "w") as outfile:
     outfile.write(str(output))
-
-
+'''
 
 for line in output.splitlines():
     if "Name" in str(line): 
         wifi_interfaces.append(line.decode("utf-8").strip().split(": ")[1])
+
+for line in output2.splitlines():
+    #print(line.decode("utf-8"))
+    wifi_interfaces2.append(line.decode("utf-8"))
+
+for x in range(0,2):
+    wifi_interfaces2.pop(x)
+
+   #wifi_interfaces2.append(line.decode("utf-8").strip().split(": ")[1])
+
+for line in wifi_interfaces2:
+    if line == '':
+        wifi_interfaces2.remove(line)
+    if "Interface name" in str(line):
+        wifi_interfaces2.remove(line)
+
+wifi_interfaces2 = wifi_interfaces2[0::2]
+wifi_interfaces2 = wifi_interfaces2[ : -1]
+for index in range(0, len(wifi_interfaces2), 2):
+    tmp_list = [wifi_interfaces2[index], wifi_interfaces2[index+1]]
+    wifi_networks.append(tmp_list)
+
+print(wifi_networks)
 
 '''
 for line in output.splitlines():
@@ -33,5 +59,5 @@ for line in output.splitlines():
 '''
 
 #printing out the list
-print(wifi_interfaces)
+#print(wifi_interfaces2[4], wifi_interfaces2[6])
 #print(wifi_interfaces[0])
