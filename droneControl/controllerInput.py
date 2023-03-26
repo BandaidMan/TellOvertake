c2df6044ea612b4c2d0d2f505fe61734ca04eab7
Aidan Kollar <aidan.kollar@my.utsa.edu>
2023-03-26 00:57:39 -0500

    Start of processing input

--- droneControl/controllerInput.py
+++ droneControl/controllerInput.py
@@ -1,3 +1,20 @@
 from djitellopy import Tello
 import pygame
 
+if a_button:
+    drone.takeoff()
+elif b_button:
+    drone.land()
+elif x_button:
+    drone.emergency()
+elif y_button:
+    # Placeholer comment
+    print("Oopsie haha")
+elif dpad_up:
+    drone.flip_forward()
+elif dpad_down:
+    drone.flip_back()
+elif dpad_left:
+    drone.flip_left()
+elif dpad_right:
+    drone.flip_right()
